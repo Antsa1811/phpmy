3fd7c7b132db379ade7b0909760c602aa2632ba2
antsa1811 <antsadev@gmail.com>
2018-12-21 20:04:19 +0300

    Creation du fichier index.php avec les code init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <title>Index pour la page php</title>
+  </head>
+  <body>
+    <div class="container margin-top">
+          <h3 class="text-center" >Teste de fichier php</h3><br/>
+        <p class="text-alert">Nous somme le <?php echo Date("j/m/Y");   ?></p>
+        
+
+
+    </div>
+
+  </body>
+</html>
