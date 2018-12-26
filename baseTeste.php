ab85e074f522a3880c8a2e4c2790ff9794e2f20d
antsa1811 <antsadev@gmail.com>
2018-12-26 20:43:56 +0300

    Ajout de baseTeste.php pour la teste base de donner

---
+++ baseTeste.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href=css/bootstrap.min.css>
+      <link rel="stylesheet" type="text/css" href=css/style.css>
+      <title>Teste base de  donner</title>
+  </head>
+  <body>
+  <div class="container margin-top ">
+      <h3 class="text-center">Teste de base de donner pour exercice php</h3>
+  </div>
+
+  </body>
+</html>
