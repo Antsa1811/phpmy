b1cb9ac31278e9f3a286dd5e19750d3cefd0d536
antsa1811 <antsadev@gmail.com>
2019-01-05 22:16:24 +0300

    optimisation et ajout de nouveaux function

--- function.php
+++ function.php
@@ -6,6 +6,7 @@ $PARAM_port='3306';
 $PARAM_nom_bd='exophp';
 $PARAM_utilisateur='root';
 $PARAM_mot_passe='';*/
+global $bdd;
 
 try
 {
@@ -19,5 +20,37 @@ catch(Exception $e)
 }
 }
 
+function verifMail($destinataire,$sujet,$message)
+{
+    $return=true;
+    $error=array();
+
+    if(empty($destinataire) || !filter_var($destinataire,FILTER_SANITIZE_STRING))
+    {
+        $return=false;
+        $error['destinataire']="Veuillez verifier votre destinataire";
+
+    }
+
+    if(empty($sujet) || !filter_var($sujet,FILTER_SANITIZE_STRING))
+    {
+        $return=false;
+        $error['sujet']="Veuillez verifier votre sujet";
+    }
+
+    if(empty($message) || !filter_var($message,FILTER_SANITIZE_STRING))
+    {
+        $return=false;
+        $error['message']="Veuillez verifier votre message";
+    }
+    return array($return,$error);
+
+}
+function xRequet($crud,$tab)
+{
+    global $bdd;
+   $req=$bdd->prepare($crud);
+   $req->execute($tab);
+}
 
  ?>
