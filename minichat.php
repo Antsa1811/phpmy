781d3ec6f6625c9e745c1970a544c1099b1b93d3
antsa1811 <antsadev@gmail.com>
2019-01-19 21:47:50 +0300

    ajout tableau de minichat

--- minichat.php
+++ minichat.php
@@ -1,5 +1,6 @@
 <?php
-require 'function.php';
+require'exeFIchier.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -13,33 +14,33 @@ require 'function.php';
 </head>
 <body>
 <div class="container margin-top ">
-    <h3 class="text-center">Teste de base de donner pour exercice php</h3>
+    <h3 class="text-center">Mini chat </h3>
     <hr/>
     <div class="margin-top">
         <div class="card bg-muted">
             <div class="card-header text-center">
-                <h3 class="p-2" >Formulaire de messagerie</h3>
+                <h3 class="p-2" >Formulaire de minichat</h3>
             </div>
             <div class="card-body">
-                <form action="" id="baseteste" method="POST">
-                    <div class="form-group">
-                        <label for="destinataire">Destinataire</label>
-                        <input type="text" name="destinataire" id="destinataire" placeholder="Le destinataire" class="form-control" required="required" maxlength="25">
-                        <small id="destinataireHelp" class="form-text text-muted">Veuillez entrer un text max de 25</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="sujet">Sujet</label>
-                        <input type="text" class="form-control" maxlength="50" name="sujet" id="sujet" placeholder="Votre sujet" required="required" >
-                        <small id="destinataireHelp" class="form-text text-muted">Veuillez entrer un text max de 25</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="message">Votre message</label>
-                        <textarea rows="5" name="message" id="message" required class="form-control"></textarea>
-                    </div>
-                    <button class="btn btn-primary" id="btnEvoyer" name="" type="submit">Envoyer</button>
-                </form>
+                <h4 class="p-1 text-center"> Listes des commentaire recents </h4>
+                    <table class="table table-bordered table-condansed table-striped">
+                        <thead class="text-center">
+                        <tr >
+                            <th class="text-center"> Pseudo </th>
+                            <th class="text-center"> Date </th>
+                            <th class="text-center"> Commentaire </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+
+                    </table>
+
+
             </div>
-            <div class="card-footer">Consulter votre adresse mail</div>
+
+            <div class="card-footer">Voir les commentaire recents</div>
         </div>
     </div>
     <hr/>
@@ -47,8 +48,6 @@ require 'function.php';
 
     <a href="index.php" class="btn btn-info">Lien pour revenir index.php</a>
 
-    <button class="btn btn-info" name="btnToast" id="btntoast" >Alert teste</button>
-    <button class="btn btn-primary" name="reset" id="reset" >Reset</button>
 </div>
 
 <!--DEBUT D INSERTION JAVASCRIPT -->
