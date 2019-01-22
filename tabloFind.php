8129a061330ccefafcdabec8f1b56051b082bfc1
antsa1811 <antsadev@gmail.com>
2019-01-22 21:32:55 +0300

    Ajout de l'affiche des resultats

--- tabloFind.php
+++ tabloFind.php
@@ -1,12 +1,35 @@
+<?php  
+require 'find.php';
+
+?>
 <table class="table table-condensand table-striped table-bordered">
     <thead>
         <tr>
-        <th>Titre</th>
-        <th>Auteur</th>
-        <th>Description</th>
+        <th class="text-center">Titre</th>
+        <th class="text-center">Auteur</th>
+        <th class="text-center">Description</th>
         </tr>
     </thead>
     <tbody>
+    	<tr>
+            <?php
+            if(isset($tablo))
+            {
+                ?>
+
+
+
+    		<td class="text-center"><?php echo $tablo[1] ?></td>
+    		<td class="text-center"><?php echo $tablo[2] ?></td>
+    		<td class="text-center"><?php echo $tablo[3] ?></td>
+            <?php
+            }
+            else
+            {
+                echo "<p class='text-center text-danger alert-danger ' >Resultat introuvable</p>";
+            }
+            ?>
+    	</tr>
     </tbody>
 
 </table>
