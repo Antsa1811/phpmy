7601cb2db8746c4c81b3a2df07288a610423f6cd
antsa1811 <antsadev@gmail.com>
2019-01-22 21:18:49 +0300

    mise a jour du requete sql

--- find.php
+++ find.php
@@ -3,16 +3,26 @@ require 'exeFichier.php';
 
 
 $filmFind=isset($_POST['filmFind']) ? $_POST['filmFind']:'';
+echo $filmFind;
 
-var_dump($filmFind);
 
 //$crud="SELECT * FROM film WHERE titre LIKE '%filmFind%' ";
 
-$crud="SELECT * FROM film WHERE titre LIKE '%filmFind%' ";
+$crud="SELECT * FROM film WHERE titre LIKE '%$filmFind%' || auteur LIKE '%$filmFind%' ";
 
 $requeste=recherche($crud);
+ if($requeste==1)
+ {
+	//$tab="SELECT * FROM FILM WHERE titre='$filmFind'";
 
-var_dump($requeste);
+	$tablo=afficheSolo($crud);
+	
+ }
+ else
+ {
+ 	echo "rien de trouver";
+ }
+
 
 
 
